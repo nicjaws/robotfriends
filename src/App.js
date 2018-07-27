ff8e7e20333c5ed0898905cbf777969c4778e376
Nicolas Corbalan <outreef@hotmail.com>
2018-07-27 14:13:23 +0200

    first robofriends commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from './SearchBox';
+import { robots } from "./robots";
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ""
+        }
+    }
+    onSearchChange = (e) => {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+            console.log(filteredRobots);
+        })
+    }
+    render() {
+        return(
+        <div className="tc">
+            <h1>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <CardList robots={this.state.robots}/>
+        </div>
+        );
+    }
+}
+
+export default App;
